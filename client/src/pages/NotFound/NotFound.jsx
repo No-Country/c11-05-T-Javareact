21301ebec0608e71baa2cf2194b35fb30a71e180
Abel Acuña <ale_lasarte@hotmail.com>
2023-05-30 18:58:46 -0300

    Merge: pagina 404

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -1,21 +1,26 @@
-
-import imageError from "../../assets/404.svg"
+import imageError from '../../assets/404.svg';
+import MyButton from '../../components/Common/MyButton/MyButton';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
 	return (
-		<div className="container max-2xl w-full m-auto mt-20 flex justify-center items-center ">
-			<div className=" mx-auto mt-10 p-2 text-yellow-700">
-				<h1 className="text-7xl font-extrabold mb-5">404</h1>
-				<h2 className="text-4xl font-extrabold mb-5 tracking-widest">Pagina no encontrada</h2>
-				<p className="text-lg tracking-widest">Lo sentimos, la página que estas <br></br>buscando no existe</p>
-				<div className="mt-10">
-					<a href="/services" className=" p-5 font-extrabold text-center rounded-full transition hover:bg-[--secondaryColor] bg-[--primaryColor] text-white w-40 cursor-pointer"> Volver al inicio </a>
+		<div className='min-h-screen flex justify-center items-center '>
+			<div className='flex gap-16 items-center mt-24'>
+				<div className='flex flex-col gap-10 text-[--secondaryColorHover]'>
+					<h1 className='text-8xl font-[GalanoBold]'>404</h1>
+					<h2 className='text-6xl font-[GalanoBold]'>Pagina no encontrada</h2>
+					<p className='text-3xl'>
+						Lo sentimos, la página que estas <br></br>buscando no existe
+					</p>
+					<MyButton typeStyle='tertiary self-end mt-16'>
+						<Link to='/'>Volver al inicio</Link>
+					</MyButton>
+				</div>
+				<div>
+					<img src={imageError}></img>
 				</div>
 			</div>
-			<div className="mx-auto">
-				<img src={imageError}></img>
-			</div>
 		</div>
-	)
+	);
 };
 export default NotFound;
